64752a0b7db657420bf773850127d2043f5c6e59
Furji UMY <furji.ft21@mail.umy.ac.id>
2022-11-10 17:10:33 +0700

    add method  add Note

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -15,6 +15,20 @@ namespace singly_linked_list
     class List
     {
         Node START;
+        public List()
+        {
+            START = null;
+        }
+        //Method untuk menambahkan sebuah node ke dalam list
+
+        public void addNote()
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nMasukkan nomor Mahasiswa:");
+            nim = Convert.ToInt32(Console.ReadLine());
+
+        }
     }
     class Program
     {
