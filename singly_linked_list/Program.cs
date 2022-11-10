46e2a538737cbc18aa57a9e04999264ea3751368
Furji UMY <furji.ft21@mail.umy.ac.id>
2022-11-10 17:05:12 +0700

    add class list for node start

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -9,6 +9,12 @@ namespace singly_linked_list
     class Node
     {
         public int noMhs;
+        public string nama;
+        public Node next;
+    }
+    class List
+    {
+        Node START;
     }
     class Program
     {
